b85b7eff9c8e488faf107afc0dd3a20372a23775
Akshay Sharma <akshaysharma.2993@gmail.com>
2024-07-08 17:33:27 +0530

    Add problem statements in 1 & 2

--- 1-reverse.js
+++ 1-reverse.js
@@ -1,8 +1,21 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+
 function reverseInt(num) {
     var reversed = num.toString().split('').reverse().join('');
     return parseInt(reversed) * Math.sign(num);
 }
 console.log(reverseInt(-15));
+
+// --- Directions
+// Given a string, return a new string with the reversed order of characters
+// --- Examples
+//   reverse('hi') === 'ih'
+
 // function reverse(str:string) {
 //     const strToArray = str.split('')
 //     return strToArray.reverse().join('')
--- 2-palindrome.js
+++ 2-palindrome.js
@@ -1,3 +1,12 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. 
+
+// --- Examples:
+//   palindrome("kayak") === true
+//   palindrome("madam") === true
+
 function isPalindrome(string) {
     let start = 0;
     let end = string.length-1
@@ -12,6 +21,10 @@ function isPalindrome(string) {
     }
     return true
 }
+
+
+// OR
+
 
 // function isPalindrome(string) {
 //     const reversed = string.split('').reverse().join('')
