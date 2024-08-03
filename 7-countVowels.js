8e836aa0a068da2c583687a2a5bd11324c572d7c
Akshay Sharma <akshaysharma.2993@gmail.com>
2024-08-03 23:59:33 +0530

    Add alternate solution to problem 7-vowel Count

--- 7-countVowels.js
+++ 7-countVowels.js
@@ -8,16 +8,29 @@
 //   vowels('why?') --> 0 
 
 
+// function vowels(str){
+//     const strArr = str.toLowerCase().split('')
+//     // console.log(strArr)
+//     let vowelCount = 0
+//     for(let i=0; i<strArr.length; i++){
+//         if(strArr[i] === 'a' || strArr[i] === 'e' || strArr[i] === 'i' || strArr[i] === 'o' || strArr[i] === 'u'){
+//             vowelCount++
+//         }
+//     }
+//     return vowelCount
+// }
+
 function vowels(str){
-    const strArr = str.toLowerCase().split('')
-    // console.log(strArr)
-    let vowelCount = 0
-    for(let i=0; i<strArr.length; i++){
-        if(strArr[i] === 'a' || strArr[i] === 'e' || strArr[i] === 'i' || strArr[i] === 'o' || strArr[i] === 'u'){
-            vowelCount++
-        }
+    const vowels = ['a','e','i','o','u']
+    str = str.toLowerCase()
+    let count = 0
+
+    for(let char of str){
+        if(vowels.includes(char)){
+            count++
+        }        
     }
-    return vowelCount
+    return count
 }
 
 console.log(vowels('How are you?'))
